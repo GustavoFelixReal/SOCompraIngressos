0372e5a04c0ce0a70d83f69645703474ff29c1fe
Gustavo Felix <gustavo.felixreal@gmail.com>
2021-09-14 21:27:00 -0300

    Código refatorado

--- src/controller/CompraController.java
+++ src/controller/CompraController.java
@@ -3,10 +3,11 @@ package controller;
 import java.util.concurrent.Semaphore;
 
 public class CompraController extends Thread {
-	
+
 	private Semaphore semaphore;
 	private int idComprador;
 	private static int qtdIngressos = 100;
+	private int qtdIngressosSolicitados = 0;
 
 	public CompraController(int idComprador, Semaphore semaphore) {
 		this.idComprador = idComprador;
@@ -15,69 +16,68 @@ public class CompraController extends Thread {
 	
 	@Override
 	public void run() {
-		logar();
+		if (logar() && escolherIngresso()) {
+			try {
+				semaphore.acquire();
+				comprarIngresso();
+				semaphore.release();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	
-	private void logar() {
+
+	private boolean logar() {
 		int tempoDeLogon = (int) ((Math.random() * 2000) + 50);
-		
+
 		try {
 			sleep(tempoDeLogon);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		if (tempoDeLogon > 1000) {
 			System.err.println("Usuário #" + idComprador + " teve problemas no login: Time Out");
-			
-		} else {
-			escolherIngresso();
+			return false;
+
 		}
-		
+
+		return true;
 	}
-	
-	private void escolherIngresso() {
+
+	private boolean escolherIngresso() {
 		int tempoDeEscolha = (int) ((Math.random() * 3000) + 1000);
-		
+
 		try {
 			sleep(tempoDeEscolha);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		if (tempoDeEscolha > 2500) {
 			System.err.println("Usuário #" + idComprador + " teve problemas com a compra: Session Expired");
-			
-		} else {
-			try {
-				semaphore.acquire();
-				
-				int qtdIngressosSolicitados = (int) ((Math.random() * 4) + 1);
-				
-				
-				if (qtdIngressosSolicitados > qtdIngressos) {
-					System.err.println("Caro usuário #" + idComprador + ", a quantidade de ingressos solicitadas (" + qtdIngressosSolicitados + ") não estão disponíveis para compra.");
-				} else {
-					comprarIngresso(qtdIngressosSolicitados);
-				}
-				
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
+			return false;
 		}
-		
-		
-		
+
+		return true;
+
 	}
-	
-	private void comprarIngresso(int qtdIngressosSolicitados) {
+
+	private boolean comprarIngresso() {
+		qtdIngressosSolicitados = (int) ((Math.random() * 4) + 1);
+
+		if (qtdIngressosSolicitados > qtdIngressos) {
+			System.err.println("Caro usuário #" + idComprador + ", a quantidade de ingressos solicitadas ("
+					+ qtdIngressosSolicitados + ") não estão disponíveis para compra.");
+			return false;
+		}
+
 		qtdIngressos -= qtdIngressosSolicitados;
-		
-		System.out.println("Usuário #" + idComprador + " comprou " + qtdIngressosSolicitados + ". [" + qtdIngressos + " ingressos disponíveis].");
-		semaphore.release();
+		System.out.println("Usuário #" + idComprador + " comprou " + qtdIngressosSolicitados + ". [" + qtdIngressos
+				+ " ingressos disponíveis].");
+
+		return true;
 	}
 
 }
